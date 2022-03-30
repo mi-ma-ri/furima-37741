3d96e826120655c08cd514a22a7526426e734bc3
mi-ma-ri <michihiros93@gmail.com>
2022-03-30 16:47:07 +0900

    rubocop実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
- 
+
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it '全ての入力ができているとき' do
