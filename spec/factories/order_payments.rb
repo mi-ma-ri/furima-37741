89261306b02f1b97c2eaa8987a2824d874149bc5
mi-ma-ri <michihiros93@gmail.com>
2022-04-10 11:50:35 +0900

    テストコード関連

---
+++ spec/factories/order_payments.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_payment do
+
+  postcode      { "123-4567" }
+  prefecture_id { Faker::Number.between(from: 1, to: 47) }
+  city          { Faker::Address.city }
+  adress        { Faker::Address.street_address }
+  phone_num     { "07043211234" }
+  token         { Faker::Internet.password(min_length: 20, max_length: 30) }
+  end
+end
