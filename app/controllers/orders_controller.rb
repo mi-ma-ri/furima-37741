f597eb4f30e3e9690ced2a3d91f39b85c7544742
mi-ma-ri <michihiros93@gmail.com>
2022-04-10 11:52:02 +0900

    コントローラー・ビュー・ルーティング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_payment = OrderPayment.new(order_params)
     if @order_payment.valid?
+       pay_item
        @order_payment.save
        redirect_to root_path
     else
@@ -24,7 +25,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_payment).permit(:postcode, :prefecture_id, :city, :adress, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_payment).permit(:postcode, :prefecture_id, :city, :adress, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
   def set_order
--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -1,15 +1,16 @@
 class OrderPayment
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :prefecture_id, :postcode, :city, :adress, :building, :phone_num
+  attr_accessor :user_id, :item_id, :prefecture_id, :postcode, :city, :adress, :building, :phone_num, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postcode, format: {with: /\A[0-9]{3}[-][0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :adress
     validates :phone_num, format: {with: /\A[0-9]+\z/, message: 'is invalid' }
+    validates :token
   end
 
   def save
